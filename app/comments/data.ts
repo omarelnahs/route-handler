4df9236ee6850ef91dc60bcdfc227a65e4bd6023
omarelnahs <omarelnahas1141@gmail.com>
2024-08-14 06:17:50 +0200

    route

---
+++ app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments =[
+    {
+        id:1,
+        text:"This is the first one"
+    },
+    {
+        id:2,
+        text:"This is the second one"
+    },
+    {
+        id:3,
+        text:"This is the third one"
+    },
+];
