ee763b0b864452f740679a237feec6a0167469fe
omarelnahs <omarelnahas1141@gmail.com>
2024-08-16 07:37:26 +0200

    headers in route handlers

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET(){
-    return new Response("Profile data!");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request : NextRequest){
+
+    const requestHeader = new Headers(request.headers);
+    const headerList = headers()
+
+    console.log(requestHeader.get("Authorization"))
+    console.log(headerList.get("Authorization"))
+
+    return new Response("<h1>Profile data!</h1>",{
+        headers:{
+            "Content-Type":"text/html"
+        }
+    });
 }
