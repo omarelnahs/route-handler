cfde1300c2940ec37b129638de4e9e8277e3b709
omarelnahs <omarelnahas1141@gmail.com>
2024-08-17 06:24:38 +0200

    cookies

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,17 +1,26 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request : NextRequest){
 
     const requestHeader = new Headers(request.headers);
     const headerList = headers()
 
+    cookies().set("resultPerPage", "20")
+    const theme = request.cookies.get("theme")
+
     console.log(requestHeader.get("Authorization"))
     console.log(headerList.get("Authorization"))
+    console.log(theme)
+    console.log(cookies().get("resultPerPage"))
+    cookies().delete("theme")
+    console.log("after delete")
+    console.log(theme)
 
     return new Response("<h1>Profile data!</h1>",{
         headers:{
-            "Content-Type":"text/html"
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
         }
     });
 }
